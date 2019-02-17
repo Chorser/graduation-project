9dfd20c82df9153004246a2f23c75594ae171f18
syj <sunyijie12@163.com>
2019-02-17 23:30:32 +0800

    update

--- Code/Echange/pages/me/me.js
+++ Code/Echange/pages/me/me.js
@@ -1,18 +1,16 @@
 // pages/me/me.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    avatar: '',
+    nickName: ''
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    this.setData({
+      avatar: wx.getStorageSync('avatar') ||'https://yunlaiwu0.cn-bj.ufileos.com/teacher_avatar.png',
+      name: wx.getStorageSync('name') || '用户A'
+    });
   },
 
   /**
