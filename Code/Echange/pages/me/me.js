6c8f0b1c51662dbb59016ee672fb3a10ab0ae496
SunYijie <sunyijie@7cgames.com>
2019-03-02 18:45:07 +0800

    update

--- Code/Echange/pages/me/me.js
+++ Code/Echange/pages/me/me.js
@@ -1,4 +1,4 @@
-var Bmob = require('../../utils/Bmob-1.6.7.min.js');
+var Bmob = require('../../utils/bmob.js');
 var app = getApp()
 
 Page({
@@ -11,15 +11,8 @@ Page({
   onLoad: function (options) {
     this.setData({
       avatar: wx.getStorageSync('avatar') ||'https://yunlaiwu0.cn-bj.ufileos.com/teacher_avatar.png',
-      name: wx.getStorageSync('name') || '用户A'
+      name: wx.getStorageSync('name') || ''
     });
-
-    Bmob.User.login('username', 'password').then(res => {
-      console.log(res)
-    }).catch(err => {
-      console.log(err)
-    });
-    console.log(Bmob);
   },
 
   navTo: function (e) {
@@ -28,54 +21,5 @@ Page({
     wx.navigateTo({
       url: _url
     })
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
 })
